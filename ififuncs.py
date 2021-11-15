8de8a3bb497c16643bb5675b124f653b957c0a9a
He Yazhou <55084036+hyz2993@users.noreply.github.com>
2021-11-15 18:01:59 +0000

    Update ififuncs.py
    thanks a million! Do you have any advice that where should I test them?

--- ififuncs.py
+++ ififuncs.py
@@ -730,11 +730,11 @@ def get_user():
     user = ''
     if user not in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'):
         user = input(
-            '\n\n**** Who are you?\nPress 1,2,3,4,5,6,7,8,9\n\n1. Aoife Fitzmaurice\n2. Gavin Martin\n3. Kieran O\'Leary\n4. Raelene Casey\n5. Noelia Romero\n6. Ana Truchanova\n7. Eoin O\'Donohoe\n8. Yujing Huang\n9. Colm Connolly\n10. Yazhou He\n11. Aoife McGrath\n'
+            '\n\n**** Who are you?\nPress 1,2,3,4,5,6,7,8,9,10,11\n\n1. Aoife Fitzmaurice\n2. Gavin Martin\n3. Kieran O\'Leary\n4. Raelene Casey\n5. Noelia Romero\n6. Ana Truchanova\n7. Eoin O\'Donohoe\n8. Yujing Huang\n9. Colm Connolly\n10. Yazhou He\n11. Aoife McGrath\n'
         )
         while user not in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'):
             user = input(
-                '\n\n**** Who are you?\nPress 1,2,3,4,5,6,7,8,9\n1. Aoife Fitzmaurice\n2. Gavin Martin\n3. Kieran O\'Leary\n4. Raelene Casey\n5. Noelia Romero\n6. Ana Truchanova\n7. Eoin O\'Donohoe\n8. Yujing Huang\n9. Colm Connolly\n10. Yazhou He\n11. Aoife McGrath\n'
+                '\n\n**** Who are you?\nPress 1,2,3,4,5,6,7,8,9,10,11\n\n1. Aoife Fitzmaurice\n2. Gavin Martin\n3. Kieran O\'Leary\n4. Raelene Casey\n5. Noelia Romero\n6. Ana Truchanova\n7. Eoin O\'Donohoe\n8. Yujing Huang\n9. Colm Connolly\n10. Yazhou He\n11. Aoife McGrath\n'
             )
     if user == '1':
         user = 'Aoife Fitzmaurice'
