f16155bf0fc79fad1efcab41219a419589797211
He Yazhou <55084036+hyz2993@users.noreply.github.com>
2021-11-16 12:48:49 +0000

    Update ififuncs.py

--- ififuncs.py
+++ ififuncs.py
@@ -734,7 +734,7 @@ def get_user():
         )
         while user not in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'):
             user = input(
-                '\n\n**** Who are you?\nPress 1,2,3,4,5,6,7,8,9,10,11,12\n\n1. Aoife Fitzmaurice\n2. Gavin Martin\n3. Kieran O\'Leary\n4. Raelene Casey\n5. Noelia Romero\n6. Ana Truchanova\n7. Eoin O\'Donohoe\n8. Yujing Huang\n9. Colm Connolly\n10. Raven Cooke\n11. Yazhou He\n12. Aoife McGrath\n'
+                '\n\n**** Who are you?\nPress 1,2,3,4,5,6,7,8,9,10,11,12\n\n 1. Aoife Fitzmaurice\n 2. Gavin Martin\n 3. Kieran O\'Leary\n 4. Raelene Casey\n 5. Noelia Romero\n 6. Ana Truchanova\n 7. Eoin O\'Donohoe\n 8. Yujing Huang\n 9. Colm Connolly\n10. Raven Cooke\n11. Yazhou He\n12. Aoife McGrath\n'
             )
     if user == '1':
         user = 'Aoife Fitzmaurice'
