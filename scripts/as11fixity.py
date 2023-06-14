32a2d41fa920822e7dc5cf9b9bfdb615176e6e43
Yazhou He <hyz2993@gmail.com>
2023-06-14 10:13:49 +0100

    as11fixity.py - add block for -MediaConchReport.xml, update dir walking structure, add  checksums display in the terminal

--- scripts/as11fixity.py
+++ scripts/as11fixity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 '''
-WORK IN PROGRESS WORKSHOP SCRIPT!!!
+VALIDATE AS-11 UK DPP MXF BY COMPARING 
+ITS CHECKSUMS FROM BOTH PACKAGE MANIFEST
+AND DPP XML <MediaChecksumValue>
 '''
 
 import sys
@@ -41,8 +43,8 @@ def count_files(starting_dir):
         aaa = False
         try:
             current_oe = dirpath.split('oe')[1][:5]
-            if current_oe[-1] == '/':
-                current_oe = current_oe[:4]
+            if current_oe[-1] == '/' or current_oe[-1] == '\\':
+                current_oe = current_oe[:-1]
             oe = True
         except IndexError:
             try:
@@ -67,17 +69,21 @@ def count_files(starting_dir):
             print 'hi'
             dicto['aaa' + previous_oe] = [filename_counter, dir_counter]
         '''
-    print(dicto)
+    print('SIP/AIP found:')
+    for i in dicto:
+        print(i, " | filecount: ", dicto[i][0], " | folder_count: ", dicto[i][1])
+    print()
     return dicto
+
 def main():
     starting_dir = sys.argv[1]
     dicto = count_files(starting_dir)
-    print(dicto, 12312312123)
     startTime = datetime.now()
     csv_report_filename = os.path.basename(starting_dir) + "_report"
     csv_report = os.path.expanduser("~/Desktop/%s.csv") % csv_report_filename
     checkfile = os.path.isfile(csv_report)
-    counter = 0
+    if checkfile is True:
+        print("CSV file already exists.\nThis will overwrite the existed CSV.\n")
     ififuncs.create_csv(
         csv_report,
         (
@@ -93,120 +99,124 @@ def main():
             'Md5_From_Xml',
             'Md5_from_Mxf',
             'Checksum_Result'
-            )
         )
-    if checkfile is True:
-        print("CSV file already exists.")
+    )
+    
+    xml_flag = 0
     for dirpath, dirss, filenames in sorted(os.walk(starting_dir)):
-        for filename in filenames:
-            if filename.endswith('.xml'):
-                if os.path.basename(dirpath) == 'supplemental':
+        if len(os.path.basename(dirpath)) == 36:
+            uuid_dir = dirpath
+            logs_dir = os.path.join(uuid_dir, 'logs')
+            log = os.path.join(logs_dir, os.path.basename(uuid_dir) + '_sip_log.log')
+            objects_dir = os.path.join(uuid_dir, 'objects')
+            objects_list = os.listdir(objects_dir)
+            manifest_basename = os.path.basename(uuid_dir) + '_manifest.md5'
+            manifest = os.path.join(os.path.dirname(uuid_dir), manifest_basename)
+        if os.path.basename(dirpath) == 'supplemental':
+            for filename in filenames:
+                if filename.endswith('.xml') and not filename.endswith('_MediaConchReport.xml'):
                     full_xml_path = os.path.join(dirpath, filename)
+                    xml_flag = 1
+                    with open(manifest, 'r', encoding='utf-8') as fo:
+                        manifest_lines = fo.readlines()
+                        for line in manifest_lines:
+                            if line.lower().replace('\n', '').endswith('.mxf'):
+                                mxf_checksum = line[:32]
+                                #mxf_checksum = str(digest_with_progress(mxf, 1024))
+                                print(mxf_checksum + ' - mxf checksum from package manifest')
+                    try:
+                        dpp_xml_parse = etree.parse(full_xml_path)
+                        dpp_xml_namespace = dpp_xml_parse.xpath('namespace-uri(.)')
+                        #parsed values from dpp xml
+                        series_title = dpp_xml_parse.findtext(
+                            '//ns:SeriesTitle',
+                            namespaces={'ns':dpp_xml_namespace}
+                        )
+                        prog_title = dpp_xml_parse.findtext(
+                            '//ns:ProgrammeTitle',
+                            namespaces={'ns':dpp_xml_namespace}
+                        )
+                        ep_num = dpp_xml_parse.findtext(
+                            '//ns:EpisodeTitleNumber',
+                            namespaces={'ns':dpp_xml_namespace}
+                        )
+                        checksum = dpp_xml_parse.findtext(
+                            '//ns:MediaChecksumValue',
+                            namespaces={'ns':dpp_xml_namespace}
+                        )
+                        print(checksum + ' - mxf checksum from DPP XML')
+                        accession_number_id = ''
+                        if os.path.isfile(log):
+                            with open(log, 'r', encoding='utf-8') as log_object:
+                                log_lines = log_object.readlines()
+                                for lines in log_lines:
+                                    if 'eventIdentifierType=object entry,' in lines:
+                                        source_oe = lines.split('=')[-1].replace('\n', '')
+                                    if 'eventIdentifierType=accession number,' in lines:
+                                        accession_number_id = lines.split('=')[-1].replace('\n', '')
+                        print('Generating Report.... ')
+                        if mxf_checksum == checksum:
+                            print(source_oe + 'Checksum MATCHES!\n---')
+                            ififuncs.append_csv(
+                                csv_report,
+                                (
+                                    os.path.basename(os.path.dirname(uuid_dir)),
+                                    source_oe,
+                                    accession_number_id,
+                                    dicto[os.path.basename(os.path.dirname(uuid_dir))][0],
+                                    dicto[os.path.basename(os.path.dirname(uuid_dir))][1],
+                                    filename,
+                                    unidecode.unidecode(series_title),
+                                    unidecode.unidecode(prog_title),
+                                    unidecode.unidecode(ep_num),
+                                    checksum,
+                                    mxf_checksum,
+                                    'CHECKSUM MATCHES!'
+                                    )
+                            )
+                        else:
+                            print(source_oe + 'Checksum does NOT MATCH!\n---'),
+                            ififuncs.append_csv(
+                                csv_report,
+                                (
+                                    os.path.basename(os.path.dirname(uuid_dir)),
+                                    source_oe,
+                                    accession_number_id,
+                                    dicto[os.path.basename(os.path.dirname(uuid_dir))][0],
+                                    dicto[os.path.basename(os.path.dirname(uuid_dir))][1],
+                                    filename,
+                                    unidecode.unidecode(series_title),
+                                    unidecode.unidecode(prog_title),
+                                    unidecode.unidecode(ep_num),
+                                    checksum,
+                                    mxf_checksum,
+                                    'CHECKSUM DOES NOT MATCH!'
+                                    )
+                            )
+                    except AttributeError:
+                        print(source_oe + 'Checksum does NOT MATCH or wrong source!\n---')
+                        ififuncs.append_csv(
+                            csv_report,
+                            (
+                                os.path.basename(os.path.dirname(uuid_dir)),
+                                source_oe,
+                                accession_number_id,
+                                dicto[os.path.basename(os.path.dirname(uuid_dir))][0],
+                                dicto[os.path.basename(os.path.dirname(uuid_dir))][1],
+                                filename,
+                                'error',
+                                'error',
+                                'error',
+                                'error',
+                                'error',
+                                'CHECKSUM DOES NOT MATCH or WRONG SOURCE!'
+                                )
+                            )
                 else:
                     continue
-                uuid_dir = os.path.dirname(os.path.dirname(dirpath))
-                objects_dir = os.path.join(uuid_dir, 'objects')
-                logs_dir = os.path.join(uuid_dir, 'logs')
-                log = os.path.join(logs_dir, os.path.basename(uuid_dir) + '_sip_log.log')
-                
-                objects_list = os.listdir(objects_dir)
-
-                manifest_basename = os.path.basename(uuid_dir) + '_manifest.md5'
-                manifest = os.path.join(os.path.dirname(uuid_dir), manifest_basename)
-                with open(manifest, 'r', encoding='utf-8') as fo:
-                    manifest_lines = fo.readlines()
-                    for line in manifest_lines:
-                        if line.lower().replace('\n', '').endswith('.mxf'):
-                        
-                            mxf_checksum = line[:32]
-                            print(mxf_checksum)
-                #mxf_checksum = str(digest_with_progress(mxf, 1024))
-                try:
-                    dpp_xml_parse = etree.parse(full_xml_path)
-                    dpp_xml_namespace = dpp_xml_parse.xpath('namespace-uri(.)')
-                    #parsed values
-                    series_title = dpp_xml_parse.findtext(
-                        '//ns:SeriesTitle',
-                        namespaces={'ns':dpp_xml_namespace}
-                    )
-                    prog_title = dpp_xml_parse.findtext(
-                        '//ns:ProgrammeTitle',
-                        namespaces={'ns':dpp_xml_namespace}
-                    )
-                    ep_num = dpp_xml_parse.findtext(
-                        '//ns:EpisodeTitleNumber',
-                        namespaces={'ns':dpp_xml_namespace}
-                    )
-                    checksum = dpp_xml_parse.findtext(
-                        '//ns:MediaChecksumValue',
-                        namespaces={'ns':dpp_xml_namespace}
-                    )
-                    accession_number_id = ''
-                    print('Generating Report....  \n')
-                    if os.path.isfile(log):
-                        print(log)
-                        with open(log, 'r', encoding='utf-8') as log_object:
-                            log_lines = log_object.readlines()
-                            for lines in log_lines:
-                                if 'eventIdentifierType=object entry,' in lines:
-                                    source_oe = lines.split('=')[-1].replace('\n', '')
-                                if 'eventIdentifierType=accession number,' in lines:
-                                    accession_number_id = lines.split('=')[-1].replace('\n', '')
-                    if mxf_checksum == checksum:
-                        print(dicto,7897897897)
-                        ififuncs.append_csv(
-                            csv_report,
-                            (
-                                os.path.basename(os.path.dirname(uuid_dir)),
-                                source_oe,
-                                accession_number_id,
-                                dicto[os.path.basename(os.path.dirname(uuid_dir))][0],
-                                dicto[os.path.basename(os.path.dirname(uuid_dir))][1],
-                                filename,
-                                unidecode.unidecode(series_title),
-                                unidecode.unidecode(prog_title),
-                                unidecode.unidecode(ep_num),
-                                checksum,
-                                mxf_checksum,
-                                'CHECKSUM MATCHES!'
-                                )
-                        )
-                    else:
-                        ififuncs.append_csv(
-                            csv_report,
-                            (
-                                os.path.basename(os.path.dirname(uuid_dir)),
-                                source_oe,
-                                accession_number_id,
-                                dicto[os.path.basename(os.path.dirname(uuid_dir))][0],
-                                dicto[os.path.basename(os.path.dirname(uuid_dir))][1],
-                                filename,
-                                unidecode.unidecode(series_title),
-                                unidecode.unidecode(prog_title),
-                                unidecode.unidecode(ep_num),
-                                checksum,
-                                mxf_checksum,
-                                'CHECKSUM DOES NOT MATCH!'
-                                )
-                        )
-                except AttributeError:
-                    ififuncs.append_csv(
-                        csv_report,
-                        (
-                            os.path.basename(os.path.dirname(uuid_dir)),
-                            source_oe,
-                            accession_number_id,
-                            dicto[os.path.basename(os.path.dirname(uuid_dir))][0],
-                            dicto[os.path.basename(os.path.dirname(uuid_dir))][1],
-                            filename,
-                            'error',
-                            'error',
-                            'error',
-                            'error',
-                            'error',
-                            'CHECKSUM DOES NOT MATCH!'
-                            )
-                        )
+            if xml_flag == 0:
+                print(dirpath + '\n*****There is no metadata xml file in the supplement directory!\n*****Check if the structure is correct.\n---')
+                continue
     print("Report complete - Time elaspsed : ", datetime.now() - startTime)
 
 
