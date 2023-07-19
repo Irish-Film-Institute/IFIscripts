e213348e2bf05771d7aef4ea2e7d95b764629335
Yazhou He <hyz2993@gmail.com>
2023-07-19 15:20:54 +0100

    lossy_check.py - add final result grouped by lossy type

--- scripts/lossy_check.py
+++ scripts/lossy_check.py
@@ -36,14 +36,13 @@ def find_packages(source):
 		elif package[:3] == 'aaa':
 			p_type = 'AIP'
 			package_list.append(package)
-		print("%s %s found in the source directory" %(p_type,package))
+		print("%s %s\tfound in the source directory" %(p_type,package))
 	return package_list
 
 def find_log_result(source, package_list):
 	results={}
 	for package in package_list:
 		package_path = os.path.join(source, package)
-		result = {package: ''}
 		uuid_path = sorted(os.listdir(package_path))
 		for uuid_dir in uuid_path:
 			if len(uuid_dir)==36 and not uuid_dir.endswith('.txt') and not uuid_dir.endswith('.md5'):
@@ -52,7 +51,11 @@ def find_log_result(source, package_list):
 		for log in logs:
 			if log.endswith('_seq2ffv1_log.log'):
 				log_path = os.path.join(log_dir, log)
+				break
+			else:
+				log_path=''
 		if os.path.isfile(log_path):
+			results[package] = 'ERROR - cannot find target in the log'
 			with open(log_path, 'r', encoding='utf-8') as log_object:
 				log_lines = log_object.readlines()
 				for lines in log_lines:
@@ -64,6 +67,8 @@ def find_log_result(source, package_list):
 						break
 					else:
 						results[package] = 'ERROR - cannot find target in the log'
+		else:
+			results[package] = 'ERROR - cannot find the log'
 	return results
 
 def main(args_):
@@ -72,8 +77,27 @@ def main(args_):
 	package_list = find_packages(source)
 	results = find_log_result(source, package_list)
 	print()
+	lossless_list=[]
+	lossy_list=[]
+	error_list=[]
 	for key in results:
 		print(key + ':\t' + results[key])
+		if results[key] == 'lossless':
+			lossless_list.append(key)
+		elif results[key] == 'lossy':
+			lossy_list.append(key)
+		else:
+			error_list.append(key)
+	print()
+	print('Lossless information packages:')
+	print(*lossless_list, sep='\n')
+	if not lossy_list and not error_list:
+		print('---\nAll inforamtion packages are lossless!')
+	else:
+		print('---\nLossy information packages:')
+		print(*lossy_list, sep='\n')
+		print(*error_list, sep='*\tERROR - log check failed\n', end='*\tERROR - log check failed\n')
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
