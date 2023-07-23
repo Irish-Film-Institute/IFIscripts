65822cadf4e92284db1052cc5265f6d4c9ca2df1
Yazhou He <hyz2993@gmail.com>
2023-07-23 23:21:25 +0100

    pyproject.toml, setup.py - update version number

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ NOTE: Objects.py has been copied from https://github.com/simsong/dfxml. walk_to_
         'scripts/framemd5.py',
         'scripts/ififuncs.py',
         'scripts/loopline_repackage.py',
+        'scripts/lossy_check.py',
         'scripts/make_mediaconch.py',
         'scripts/makedfxml.py',
         'scripts/makedip.py',
@@ -62,6 +63,7 @@ NOTE: Objects.py has been copied from https://github.com/simsong/dfxml. walk_to_
         'scripts/seq2ffv1.py',
         'scripts/sipcreator.py',
         'scripts/strongbox_fixity.py',
+        'scripts/structure_check.py',
         'scripts/subfolders.py',
         'scripts/testfiles.py',
         'scripts/validate.py',
@@ -78,6 +80,6 @@ NOTE: Objects.py has been copied from https://github.com/simsong/dfxml. walk_to_
     data_files=[('', ['film_scan_aip_documentation.txt', '26_XYZ-22_Rec709.cube'])],
     include_package_data=True,
     name='ifiscripts',
-    version='2023.7.14a2',
+    version='2023.7.23',
     python_requires='>=3.8'
 )
